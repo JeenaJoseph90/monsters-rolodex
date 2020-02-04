e172b9f15cc339c577ec3c61248a88c6815d4590
JeenaJoseph90 <45370725+JeenaJoseph90@users.noreply.github.com>
2020-02-04 19:38:52 +0400

    Updates

---
+++ precache-manifest.dd336a3aa2de62da386ec8a775b15b85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599bfa9f15b219f9b2157c949b48c2da",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0a92e2ee0ec07230487f",
+    "url": "/monsters-rolodex/static/css/main.bf9aa67d.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a92e2ee0ec07230487f",
+    "url": "/monsters-rolodex/static/js/main.2c8f76b0.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
